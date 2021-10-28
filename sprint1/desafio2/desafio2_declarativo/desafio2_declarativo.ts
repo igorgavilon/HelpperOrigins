ef5d7887c7cec95bc64328a4e39cad4bc4be810f
igorgavilon <igorgavilon@gmail.com>
2021-10-27 22:20:41 -0400

    resolução do desafio 2 imperativo e declarativo

---
+++ sprint1/desafio2/desafio2_declarativo/desafio2_declarativo.ts
@@ -0,0 +1,88 @@
+//definição do tipo de objeto que será utilizado em Array<Scientist>
+class Scientist {
+    id?: number
+    name?: string
+    bio?: string
+}
+
+//declaração do array de Objetos
+let lista2 = new Array<Scientist>(
+    {id : 1, name: "Ada Lovelace", 
+        bio : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
+    {id : 2, name: "Alan Turing", 
+        bio : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
+    {id : 3, name: "Nikola Tesla", 
+        bio : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
+    {id : 4, name: "Nicolau Copérnico", 
+        bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
+);
+
+//retorna true caso element.id == id passado para a função
+//será utilizado na função filter()
+const equalId = (id: number) => (element: Scientist) => element.id == id;
+
+//função que retorna a bio do id passado
+function getBio(id: number) {
+    //array que armazenará o resultado do filtro
+    let result = new Array<Scientist>();
+    //filtra o objeto com id passado para essa função
+    result = lista2.filter(equalId(id));
+    //verifica se o filtro retornou algum objeto e retorna a bio
+    //senão o id não foi encontrado
+    return (result.length == 0 ? 'id não encontrado.' : result[0].bio);
+}
+
+//função que retorna o nome do id passado
+function getName(id: number) {
+    //array que armazenará o resultado do filtro
+    let result = new Array<Scientist>();
+    //filtra o objeto com id passado para essa função
+    result = lista2.filter(equalId(id));
+    //verifica se o filtro retornou algum objeto e retorna o name
+    //senão o id não foi encontrado
+    return (result.length == 0 ? 'id não encontrado.' : result[0].name);
+}
+
+//retorna true caso o element.id seja diferente do id passado para a função
+//será utilizado na função filter()
+const diferentId = (id: number) => (element: Scientist) => element.id != id;
+
+//função que remove um objeto da lista pelo id
+function deleteById(id: number) {
+    //array que armazenará o resultado do filtro
+    let listResult = new Array<Scientist>();
+    //filtra o array e retira somente o objeto com o id passado
+    listResult = lista2.filter(diferentId(id));
+    //atualiza a variável lista2 após a exclusão do objeto
+    lista2 = listResult;
+}
+
+//função que altera o valor de uma propriedade selecionada por parâmetro
+//será utilizada pela função map()
+const alterById = (property: string, newValue: string) => (element: Scientist) => element[property] = newValue;
+
+//função que altera o valor da propriedade 'name' ou 'bio' do id passado
+//para alterar 'name': property = 'name'. para alterar 'bio': property = 'bio'
+function updateById(id: number, property: string, newValue: string) {
+    //faz uma busca do objeto pelo id, caso encontre altera o valor da propriedade 'nome' ou 'bio'
+    lista2.filter(equalId(id)).map(alterById(property, newValue))
+}
+
+//realizando buscas para o id especificado
+console.log(getName(3));
+console.log(getBio(3));
+//deletando o id especificado
+deleteById(3);
+//testando se o id foi deletado
+//espera-se a seguinte resposta: id não encontrado
+console.log(getName(3));
+
+
+
+//alterando as propriedades do id 1
+updateById(1, 'name', 'Igor Gavilon');
+updateById(1, 'bio', 'Desenvolvedor de Software');
+//imprime a lista para verificar que os dados foram realmente alterados
+console.log(lista2);
+
+
---
+++ sprint1/desafio2/desafio2_imperativo/desafio2_imperativo.ts
@@ -0,0 +1,101 @@
+//definição do tipo de objeto que será utilizado em Array<Scientist>
+class Scientist {
+    id?: number
+    name?: string
+    bio?: string
+}
+
+//declaração do array de Objetos
+let lista = new Array<Scientist>(
+    {id : 1, name: "Ada Lovelace", 
+        bio : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
+    {id : 2, name: "Alan Turing", 
+        bio : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
+    {id : 3, name: "Nikola Tesla", 
+        bio : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
+    {id : 4, name: "Nicolau Copérnico", 
+        bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
+);
+
+//função que retorna a bio do id passado
+function getBio(id: number) {
+    //define um padrão de resposta caso não encontre um id válido
+    let response = 'Nenhum id encontrado.'
+    //busca entre os elementos do Array um objeto com o id
+    lista.forEach(element => {
+        if(element.id == id) {
+            //caso encontre, a response será a bio
+            response = element.bio!;
+        }
+    });
+    return (response);
+    
+}
+
+//função que retorna o nome do id passado
+function getName(id: number) {
+    //define um padrão de resposta caso não encontre um id válido
+    var response = 'Nenhum id encontrado.'
+    //busca entre os elementos do Array um objeto com o id
+    lista.forEach(element => {
+        if(element.id == id) {
+            //caso encontre, a response será o name
+            response = element.name!;
+        }
+    });
+    return (response);
+}
+
+//função que remove um objeto da lista pelo id
+function deleteById(id: number) {
+    //Array que guardará somente os objetos que não serão excluídos
+    var temporaryList = new Array<Scientist>();
+    //busca pelos objetos que não serão excluídos
+    //e guarda em temporaryList
+    lista.forEach(element => {
+        if(element.id != id) {
+            temporaryList.push(element);
+        }
+    });
+    //atualiza a lista após a exclusão do objeto selecionado pelo id
+    lista = temporaryList;
+}
+
+//função que altera o valor da propriedade 'name' ou 'bio' do id passado
+//para alterar 'name': property = 'name'. para alterar 'bio': property = 'bio'
+function updateById(id: number, property: string, newValue: string) {
+    //resposta padrão caso não encontre um id válido
+    var response = 'Nenhum id encontrado.'
+    //busca pelo elemento que será alterado
+    lista.forEach(element => {
+        if(element.id == id) {
+            //verifica qual propriedade deve ser alterada
+            if(property == 'name') {
+                element.name = newValue;
+                response = 'Propriedade Name alterada.'
+            }else if(property == 'bio') {
+                element.bio = newValue;
+                response = 'Propriedade Bio alterada.'
+            }
+        }
+    });
+    //retorna uma resposta informando se a operação foi efetuada ou não
+    return (response);
+}
+
+//realizando buscas para o id especificado
+console.log(getName(3));
+console.log(getBio(3));
+//deletando o id especificado
+deleteById(3);
+//testando se o id foi deletado
+console.log(getName(3));
+
+//alterando as propriedades do id 1
+updateById(1, 'name', 'igor gavilon');
+updateById(1, 'bio', 'desenvolvedor de software');
+
+//imprime a lista para verificar que os dados foram realmente alterados
+console.log(lista);
+
+
